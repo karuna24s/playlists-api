dbe690b928e96f2d201aef666b2e42de11bcbf80
karuna24s <karuna24s@gmail.com>
2018-04-11 20:21:44 -0400

    create api endpoint for creating a new playlist

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -1,8 +1,20 @@
 module Api::V1
   class PlaylistsController < ApplicationController
     def index
-      @playlists = Playlist.all
+      @playlists = Playlist.order("created_at DESC")
       render json: @playlists
     end
+
+    def create
+      @playlist = Playlist.create(playlist_params)
+      render json: @playlist
+    end
+
+    private
+
+    def playlist_params
+      params.require(:playlist).permit(:title, :body)
+    end
+
   end
 end
